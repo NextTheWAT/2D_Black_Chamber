b358ca3699c8c9b3035e097e172f0c23ce98ee97
Lee JaeEun <62467424+NextTheWAT@users.noreply.github.com>
2025-09-11 19:51:29 +0900

    [Feat] Damage Interface 추가

--- Assets/02. Scripts/Weapon/Bullet/Bullet.cs
+++ Assets/02. Scripts/Weapon/Bullet/Bullet.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private float dmg;
+
+    [Header("Hit Filter")]
+    [SerializeField] private LayerMask damageLayers = ~0; // 기본: 전부 허용
+    //[SerializeField] private bool useTagFilter = false;
+    //[SerializeField] private string[] damageTags;         // 예: "Enemy", "Player"
+
+    private int dmg;
     private float life;
     private float spawnTime;
     private int ignoreLayer;
@@ -16,7 +22,7 @@ public class Bullet : MonoBehaviour
         col.isTrigger = true; // 간단하게 Trigger 충돌만
     }
 
-    public void Init(Vector2 position, Vector2 dir, float speed, float damage, float lifetime, int ignoreLayer)
+    public void Init(Vector2 position, Vector2 dir, float speed, int damage, float lifetime, int ignoreLayer)
     {
         transform.position = position;
         float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
@@ -37,10 +43,32 @@ public class Bullet : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        //1. 발사자 레이어는 무시
         if (other.gameObject.layer == ignoreLayer) return;
 
-        // 상대 오브젝트에 TakeDamage(float) 있으면 데미지 전달
-        other.SendMessage("TakeDamage", dmg, SendMessageOptions.DontRequireReceiver);
+        //2. 레이어 필터링
+        int otherLayerBit = 1 << other.gameObject.layer;
+        if((damageLayers.value & otherLayerBit) == 0) return; // 데미지 레이어 필터링
+
+        // 3) 태그 필터
+        //if (useTagFilter && (damageTags == null || damageTags.Length == 0))
+        //    return;
+        //if (useTagFilter)
+        //{
+        //    bool tagOK = false;
+        //    for (int i = 0; i < damageTags.Length; i++)
+        //    {
+        //        if (other.CompareTag(damageTags[i])) { tagOK = true; break; }
+        //    }
+        //    if (!tagOK) return;
+        //}
+
+        //인터페이스로 데미지 전달
+        IDamageable target = other.GetComponent<IDamageable>();
+        if (target != null)
+        {
+            target.TakeDamage(dmg);
+        }
 
         Destroy(gameObject);
     }
