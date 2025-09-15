a3c9e6c239ebaa428da1a9c043242a1b4db796f3
DMDZ99 <ldhdo430@naver.com>
2025-09-15 15:23:47 +0900

    [Feat]입구콜라이더 및 페이즈에 따른 탈출구 오픈

--- Assets/02. Scripts/Exit/ExitPortal.cs
+++ Assets/02. Scripts/Exit/ExitPortal.cs
@@ -7,31 +7,44 @@ public class ExitPortal : MonoBehaviour
 {
     public GameObject defaultLight;
     public GameObject clearLight;
+
+    [SerializeField]
     public string clearSceneName = "";      // 클리어씬
-    private bool npcKilled = false;
 
     private void Start()
     {
         defaultLight.SetActive(true);
         clearLight.SetActive(false);
+
+        MissionManager.Instance.OnPhaseChanged += ExitPhaseChanged; // 페이즈체인지일때 Exit페이즈체인지도 적용
     }
-    public void isNpcKilled()
+
+    private void OnDestroy()
     {
-        Debug.Log("클리어 조건을 만족하였습니다.");
-        npcKilled = true;
-
-        defaultLight.SetActive(false);
-        clearLight.SetActive(true);
-        Debug.Log("탈출 가능");
+        if(MissionManager.Instance != null)
+            MissionManager.Instance.OnPhaseChanged -= ExitPhaseChanged; // 파괴될때 Exit페이즈체인지도 파괴
     }
 
-    // 연구원이 맵에 없을때 열리게
+    private void ExitPhaseChanged(MissionPhase phase)
+    {
+        if (phase == MissionPhase.Escape)
+        {
+            if (defaultLight != null)
+                defaultLight.SetActive(false);
+            if (clearLight != null)
+                clearLight.SetActive(true);
+            Debug.Log("탈출 가능");
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player") && npcKilled)  // npc태그? 연구원만 적용
+        if (other.CompareTag("Player") && MissionManager.Instance.Phase == MissionPhase.Escape)
         {
             Debug.Log("클리어하였습니다.");
+
+            MissionManager.Instance.SetPhase(MissionPhase.Completed);   // 페이즈 클리어로 바꿔주기
+
             SceneManager.LoadScene(clearSceneName);
         }
         else
