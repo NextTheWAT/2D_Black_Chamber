3a5d2932fd0a5b4a17acbdb932af8758c1a79638
khh7052 <59150989+khh7052@users.noreply.github.com>
2025-09-17 19:46:28 +0900

    [Fix] 적 추적 상태 제거

--- Assets/02. Scripts/Enemy/State/FSM/SoliderFSM.cs
+++ Assets/02. Scripts/Enemy/State/FSM/SoliderFSM.cs
@@ -8,7 +8,6 @@ public class SoliderFSM : StateMachine
     public SoliderFSM(Enemy owner, StateTable stateTable) : base(owner, stateTable)
     {
         InvestigateState investigateState = GetState<InvestigateState>();
-        ChaseState chaseState = GetState<ChaseState>();
         AssaultState assaultState = GetState<AssaultState>();
         AttackState attackState = GetState<AttackState>();
         // 잠입
@@ -19,9 +18,7 @@ public class SoliderFSM : StateMachine
 
         // Patrol
         AddTransition<PatrolState, SuspectState>(() => owner.HasSuspiciousTarget); // 근처에 타겟 있으면 의심
-        AddTransition<PatrolState, ChaseState>(() => owner.HasTarget); // 타겟 발견하면 공격
         AddTransition<PatrolState, CoverState>(() => GameManager.Instance.IsCombat); // 난전 시작되면 엄폐
-
 
         // Suspect
         AddTransition<SuspectState, InvestigateState>(() => !owner.HasSuspiciousTarget); // 근처에 타겟 없으면 다시 순찰
@@ -33,20 +30,14 @@ public class SoliderFSM : StateMachine
         AddTransition<InvestigateState, ReturnState>(() => !investigateState.IsInvestigating); // 조사 시간 끝나면 복귀
 
         // Return
-        AddTransition<ReturnState, ChaseState>(() => owner.HasTarget);
         AddTransition<ReturnState, PatrolState>(() => owner.IsArrived);
 
         // 난전
 
-        // Chase
-        AddTransition<ChaseState, CoverState>(() => chaseState.IsTargetInChaseRange); // 사정거리 안에 들어오면 공격
-
         // Cover
-        // AddTransition<CoverState, ChaseState>(() => !chaseState.IsTargetInChaseRange); // 사정거리 벗어나면 추격
         AddTransition<CoverState, AttackState>(() => attackState.IsTargetInAttackRange && owner.IsTargetInSight); // 사정거리 안에 들어오면 공격
 
         // Assault
-        AddTransition<AssaultState, ChaseState>(() => !chaseState.IsTargetInChaseRange); // 사정거리 벗어나면 추격
         AddTransition<AssaultState, AttackState>(() => owner.IsTargetInSight); // 공격 가능하면 공격
 
         // Attack
