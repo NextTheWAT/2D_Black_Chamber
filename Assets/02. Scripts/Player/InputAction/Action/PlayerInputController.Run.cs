32592ca160657ccfc7cfe1c4552845834051ea90
Lee JaeEun <62467424+NextTheWAT@users.noreply.github.com>
2025-09-18 16:01:11 +0900

    [Feat] Player Run 추가

--- Assets/02. Scripts/Player/InputAction/Action/PlayerInputController.Run.cs
+++ Assets/02. Scripts/Player/InputAction/Action/PlayerInputController.Run.cs
@@ -1,10 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public partial class PlayerInputController : TopDownController
 {
-    public void OnRun(InputAction.CallbackContext ctx) { /* 필요 시 */ }
+    [SerializeField] private PlayerConditionManager condition; // 같은 오브젝트에 붙이길 권장
+    private bool runHeld;
+    public bool RunHeld => runHeld;
 
+    public void OnRun(InputAction.CallbackContext ctx)
+    {
+        if (ctx.phase == InputActionPhase.Started) runHeld = true;
+        else if (ctx.phase == InputActionPhase.Canceled) runHeld = false;
+        // 달리기 가능 여부 체크는 이동 로직(TopDownMovement) 쪽에서 최종 결정
+    }
 }
