d2ffc10a05c0bf0dcaef3058fc9aeee39ed4feb7
유경 <saa1007@naver.com>
2025-09-22 17:49:31 +0900

    [Remove] UI 삭제

--- Assets/02. Scripts/Player/InputAction/PlayerInputController.cs
+++ Assets/02. Scripts/Player/InputAction/PlayerInputController.cs
@@ -5,14 +5,12 @@ public partial class PlayerInputController : TopDownController
 {
     [SerializeField] Shooter shooter;
     private CharacterAnimationController animationController;
-    private WeaponSwitchCoordinator weaponSwitchCoordinator;
 
     private void Awake()
     {
         _camera = Camera.main;
         shooter = GetComponent<Shooter>();
         animationController = GetComponent<CharacterAnimationController>();
-        weaponSwitchCoordinator = GetComponent<WeaponSwitchCoordinator>();
     }
 
     private void LateUpdate()
