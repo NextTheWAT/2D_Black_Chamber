5ae11fd422f63a33b0776eaa9c87712460a71a47
유경 <saa1007@naver.com>
2025-09-23 17:58:09 +0900

    [Feat] 로비씬 UI 추가

---
+++ Assets/02. Scripts/UI/Base/SceneInitializer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+// 씬이 시작될 때, 이 씬에서 보여야 할 UIKey를 UIRoot에 알려주는 초기화 스크립트.
+public class SceneInitializer : MonoBehaviour
+{
+    [SerializeField] private UIRoot uiRoot; // UIRoot 프리팹 참조
+    [SerializeField] private UIKey activeUI; // 이 씬에서 보여줄 Canvas 선택
+
+    private void Start()
+    {
+        if (uiRoot != null)
+            uiRoot.ShowOnly(activeUI);
+        else
+            Debug.LogError("[SceneInitializer] uiRoot 참조가 비였음");
+    }
+}
---
+++ Assets/02. Scripts/UI/Base/UIBase.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public abstract class UIBase : MonoBehaviour
+{
+    public bool IsOpen { get; private set; }
+    protected bool Initialized;
+
+    public virtual void OpenUI()
+    {
+        gameObject.SetActive(true);
+        IsOpen = true;
+        OnOpen();
+    }
+
+    public virtual void CloseUI()
+    {
+        OnClose();
+        IsOpen = false;
+        gameObject.SetActive(false);
+    }
+
+    protected virtual void OnOpen() { }
+    protected virtual void OnClose() { }
+}
