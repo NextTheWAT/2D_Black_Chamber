fc26edc294db2e3ef2422e57cf6c4bbe2c78cb06
lee-hyeonsu-1 <dbsdnrl123@gmail.com>
2025-09-24 20:09:18 +0900

    feat] 탄창 스크립트 추가중

--- Assets/02. Scripts/Magazine/Magazine.cs
+++ Assets/02. Scripts/Magazine/Magazine.cs
@@ -2,17 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Magazine : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public interface IInteractable
     {
-        
+        void Interaction(GameObject player);
     }
 
-    // Update is called once per frame
-    void Update()
+    //public int health = 100;
+    public GameObject ammoPickupPrefab;  // 인터랙션 프리팹
+
+    //public void TakeDamage(int damage)
+    //{
+    //    health -= damage;
+    //    if (health <= 0)
+    //    {
+    //        Die();
+    //    }
+    //}
+
+    void Die()
     {
-        
+        // 적 위치에 탄창 프리팹 생성
+        Instantiate(ammoPickupPrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject); // 적 사라짐
     }
 }
+
