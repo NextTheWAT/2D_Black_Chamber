32a8650e18eb874c7029c9a44655be8b9cf7427e
lee-hyeonsu-1 <dbsdnrl123@gmail.com>
2025-09-25 19:09:45 +0900

    fix] 아이템 스크립트 수정

--- Assets/02. Scripts/Item/Item.cs
+++ Assets/02. Scripts/Item/Item.cs
@@ -1,16 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SimpleItem : MonoBehaviour
 {
-    [Header("아이템 설정")]
-    public string itemName;    // 아이템 이름
-    public float pickupRange = 2f;        // 플레이어 접근 감지 거리
+    public string itemName;
+    public float pickupRange;  // 플레이어 접근 감지 거리
 
-    private Transform player;             // 플레이어 Transform
-    private bool playerInRange = false;   // 플레이어가 근처에 있는지 여부
-
+    private Transform player;    // 플레이어 Transform
+    private bool playerInRange = false; // 플레이어가 근처에 있는지 여부
+    public GameObject dropItem;  //드랍 프리팹
     void Start()
     {
         // Inspector에 player를 안 넣으면 씬에서 자동으로 찾기
@@ -29,7 +29,7 @@ public class SimpleItem : MonoBehaviour
         float distance = Vector3.Distance(transform.position, player.position);
         playerInRange = distance <= pickupRange;
 
-        // 범위 안이고 E 키를 눌렀을 때 아이템 줍기
+        
         if (playerInRange && Input.GetKeyDown(KeyCode.F))
         {
             PickUp();
@@ -38,16 +38,17 @@ public class SimpleItem : MonoBehaviour
 
     void OnDrawGizmosSelected()
     {
-        // Scene 뷰에서 범위 표시 (디버그용)
+        // Scene 뷰에서 범위 표시
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(transform.position, pickupRange);
     }
 
     public void PickUp()
     {
-        Debug.Log(itemName + "을(를) 주웠습니다!");
-        Destroy(gameObject); // 아이템 삭제
+        Debug.Log(itemName + "주웠습니다");
+        Destroy(gameObject);
     }
+
 }
 
 
