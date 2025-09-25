b6c62309ffe0dc5a83ba8d119e5a7ec2ec8191fd
DMDZ99 <ldhdo430@naver.com>
2025-09-25 19:32:48 +0900

    [Feat]머니스포너미완

--- Assets/02. Scripts/Manager/Money/MoneySpawner.cs
+++ Assets/02. Scripts/Manager/Money/MoneySpawner.cs
@@ -21,6 +21,20 @@ public class MoneySpawner : MonoBehaviour
 
     private void SpawnMoney()
     {
-        if (spawnPoint.Count < spawnCount) spawnCount = spawnerPoint.Count;
+        if (spawnerPoint == null || spawnerPoint.Count == 0)
+            return;
+
+        List<MoneySpawnZone> randomList = new List<MoneySpawnZone>(spawnerPoint);
+        for (int i = 0; i < spawnerPoint.Count; i++)
+        {
+            // 리스트에 있는 스폰포인트들 중 랜덤 스폰카운트개수만큼 선택
+            MoneySpawnZone random = randomList[i];
+            int randomIndex = Random.Range(i, randomList.Count);
+            randomList[i] = randomList[randomIndex];
+            randomList[randomIndex] = random;
+            // 프리팹을 생성
+
+            // 금액에따른 프리팹이 생성되게 구분
+        }
     }
 }
