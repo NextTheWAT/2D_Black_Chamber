94caef14267424481db4ef2f608584ddd1616b28
lee-hyeonsu-1 <dbsdnrl123@gmail.com>
2025-10-01 15:40:21 +0900

    feat]권총,라이플 탄창 각각 드랍

--- Assets/02. Scripts/Item/Item.cs
+++ Assets/02. Scripts/Item/Item.cs
@@ -6,7 +6,15 @@ using UnityEngine;
 
 public class Item : MonoBehaviour, Iinteraction
 {
+    public enum WeaponType  // 무기 타입
+    {
+        Gun,
+        Rifle
+    }
+
     public string itemName;
+    public WeaponType weaponType; // 권총/라이플 구분
+    public int ammoAmount;
 
     private GameObject player;    // 플레이어 Transform
     //private bool playerInRange = false; // 플레이어가 근처에 있는지 여부
@@ -15,38 +23,39 @@ public class Item : MonoBehaviour, Iinteraction
         // Inspector에 player를 안 넣으면 씬에서 자동으로 찾기
         if (!player)
         {
-            player  = GameObject.FindGameObjectWithTag("Player");
+            player = GameObject.FindGameObjectWithTag("Player");
             //GameObject p = GameObject.FindGameObjectWithTag("Player");
             //if (p) player = p.transform;
         }
     }
 
-    void OnDrawGizmosSelected()
-    {
-        // Scene 뷰에서 범위 표시
-        //Gizmos.color = Color.yellow;
-        //Gizmos.DrawWireSphere(transform.position, pickupRange);
-    }
-    
+   
     public void Interaction()
     {
         PickUp();
     }
-    public void PickUp()
+
+    public void PickUp()    // 탄창 권총, 라이플 구별해서 드랍
     {
         Debug.Log(itemName + "을 주웠습니다");
 
-        //탄창 추가
         Shooter shooter = player.GetComponentInChildren<Shooter>();
         if (shooter != null)
         {
-            shooter.AddAmmo(12);
+            switch (weaponType)
+            {
+                case WeaponType.Gun:
+                    shooter.AddAmmo(ammoAmount);
+                    break;
+                case WeaponType.Rifle:
+                    shooter.AddAmmo(ammoAmount);
+                    break;
+            }
         }
 
         Destroy(gameObject);
     }
 
 }
-
 
 
