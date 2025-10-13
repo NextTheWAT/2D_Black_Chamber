a173cd5507df0d909186f920371483c9ef0aafba
DMDZ99 <ldhdo430@naver.com>
2025-10-13 16:46:49 +0900

    [Feat]탭버튼추가및수정

--- Assets/02. Scripts/Store/StoreTabBtn.cs
+++ Assets/02. Scripts/Store/StoreTabBtn.cs
@@ -4,28 +4,41 @@ using UnityEngine;
 
 public class StoreTabBtn : MonoBehaviour
 {
-    public GameObject weaponsCanvas;
-    public GameObject toolsCanvas;
+    public GameObject weaponsPanel;
+    public GameObject toolsPanel;
+
     public GameObject weaponsButtonBar;
     public GameObject toolsButtonBar;
 
     private void Start()
     {
-        ShowWeapons();
+        weaponsPanel.SetActive(true);
+        toolsPanel.SetActive(false);
+
+        weaponsButtonBar.SetActive(true);
+        toolsButtonBar.SetActive(false);
     }
 
     public void ShowWeapons()
     {
-        weaponsCanvas.SetActive(true);
-        toolsCanvas.SetActive(false);
+        if (weaponsPanel.activeSelf)
+            return;
+
+        weaponsPanel.SetActive(true);
+        toolsPanel.SetActive(false);
+
         weaponsButtonBar.SetActive(true);
         toolsButtonBar.SetActive(false);
     }
 
     public void ShowTools()
     {
-        weaponsCanvas.SetActive(false);
-        toolsCanvas.SetActive(true);
+        if (toolsPanel.activeSelf)
+            return;
+
+        weaponsPanel.SetActive(false);
+        toolsPanel.SetActive(true);
+
         weaponsButtonBar.SetActive(false);
         toolsButtonBar.SetActive(true);
     }
