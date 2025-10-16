391850c554536847f1d45a7efb27b602e83b9d94
DMDZ99 <ldhdo430@naver.com>
2025-10-16 11:30:49 +0900

    [Feat]CardKey씬마다초기화

--- Assets/02. Scripts/Door/CardKey.cs
+++ Assets/02. Scripts/Door/CardKey.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CardKey : MonoBehaviour, Iinteraction
 {
     public static bool hasCardKey = false;
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode) // 씬초기화와 재시작때도 초기화되게
+    {
+        hasCardKey = false;     // 카드키 초기화
+    }
 
     public void Interaction(Transform interactor)
     {
