3e8c733f307d4d300f0dadbd570a514aa92f2834
khh7052 <59150989+khh7052@users.noreply.github.com>
2025-10-17 10:32:35 +0900

    [Feat] 아이템 흭득 시 사운드 적용

--- Assets/02. Scripts/Manager/Sound/Struct/StructSoundManager.cs
+++ Assets/02. Scripts/Manager/Sound/Struct/StructSoundManager.cs
@@ -4,10 +4,12 @@ public class StructSoundManager : SoundManagerBase<StructSoundManager>
 {
     [Header("Strcut SFX")]
     public SoundData cctvBrokenSound;
+    public SoundData pickUpSound;
 
 
     [Range(0, 1)] public float structVol = 0.8f;
 
     public void PlayStructBrokenSound() => PlayRandom(cctvBrokenSound, structVol);
+    public void PlayPickUpSound() => PlayRandom(pickUpSound, structVol);
 
 }
