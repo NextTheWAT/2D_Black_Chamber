dfc5acd575b4b08a62574cc83711a05de5c8c37e
DMDZ99 <ldhdo430@naver.com>
2025-10-21 10:50:52 +0900

    [Feat]구매창, 구매완료 잔액부족 분리

--- Assets/02. Scripts/Store/PurchasePopup.cs
+++ Assets/02. Scripts/Store/PurchasePopup.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class PurchasePopup : MonoBehaviour
 {
     public GameObject purchasePanel;
-    public GameObject shortagePanel;
-    public GameObject completePanel;
 
     private void Start()
     {
@@ -16,29 +14,6 @@ public class PurchasePopup : MonoBehaviour
     public void ShowPurchasePopup()
     {
         purchasePanel.SetActive(true);
-    }
-
-    public void EnoughMoneyPopup(bool hasEnoughMoney)   // 가진금액에따라 다른 팝업뜨게 (추후 수정)
-    {
-        if (hasEnoughMoney)
-            completePanel.SetActive(true);         // 잔액 충분할때 구매시 구매완료창 뜨게
-        else
-            shortagePanel.SetActive(true);
-    }
-
-    public void ShowCompletePopup()
-    {
-        completePanel.SetActive(true);
-    }
-
-    public void CloseShortagePopup()
-    {
-        shortagePanel.SetActive(false);
-    }
-
-    public void CloseCompletePopup()
-    {
-        completePanel.SetActive(false);
     }
 
     public void ClosePurchasePopup()
