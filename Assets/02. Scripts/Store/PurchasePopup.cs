7ee26c0af587706c0532c0cecc4e3ecc1ce42b61
DMDZ99 <ldhdo430@naver.com>
2025-10-21 10:23:24 +0900

    [Fix]구매창스크립트수정

--- Assets/02. Scripts/Store/PurchasePopup.cs
+++ Assets/02. Scripts/Store/PurchasePopup.cs
@@ -18,24 +18,11 @@ public class PurchasePopup : MonoBehaviour
 
     public void ShowPurchasePopup()
     {
-        // 슬롯 버튼 누를 시 구매창 뜨게 -> 짧은딜레이를 줘서 오히려 버튼 씹힘을 없애기
-        StartCoroutine(ShowPurchasePopupDelayed());
-    }
-
-    private IEnumerator ShowPurchasePopupDelayed()
-    {
-        yield return null;
         purchasePanel.SetActive(true);
     }
 
     public void EnoughMoneyPopup(bool hasEnoughMoney)   // 가진금액에따라 다른 팝업뜨게 (추후 수정)
     {
-        StartCoroutine(ShowEnoughMoneyPopup(hasEnoughMoney));
-    }
-
-    private IEnumerator ShowEnoughMoneyPopup(bool hasEnoughMoney)
-    {
-        yield return null;
         if (hasEnoughMoney)
             completePanel.SetActive(true);         // 잔액 충분할때 구매시 구매완료창 뜨게
         else
@@ -44,12 +31,6 @@ public class PurchasePopup : MonoBehaviour
 
     public void ShowCompletePopup()
     {
-        StartCoroutine(ShowCompletePopupDelayed());
-    }
-
-    private IEnumerator ShowCompletePopupDelayed()
-    {
-        yield return null;
         completePanel.SetActive(true);
     }
 
