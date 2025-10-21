b6bb454f20db9377d1ed61144529faf5c9b4aef8
lee-hyeonsu-1 <dbsdnrl123@gmail.com>
2025-10-21 19:43:32 +0900

    faet] 무기방 이미지 슬라이드 효과 추가

--- Assets/02. Scripts/Gun image/Gunimage.cs
+++ Assets/02. Scripts/Gun image/Gunimage.cs
@@ -9,7 +9,9 @@ public class Gunimage : MonoBehaviour
     //public Sprite[] sprites;
     private Image image;
     private int currentIndex = 0;
-    // 잠시
+
+    [SerializeField]
+    private float slideDistance = 0f; // 원하는 슬라이드 거리 (픽셀)
 
     public RectTransform[] panels; // 전환 대상 패널들
     public float slideDuration = 0.5f; // 슬라이드 애니메이션 지속 시간
@@ -18,42 +20,21 @@ public class Gunimage : MonoBehaviour
 
     [SerializeField] private Button nextButton;
     [SerializeField] private Button prevButton;
-    void Awake()
+
+    public void clickshowpanel(bool plus)  //클릭 시 계속 넘어감
     {
-        image = GetComponent<Image>(); 
-        //image.sprite = sprites[currentIndex]; // 시작 시 첫 번째 이미지 표시
+        int targetindex = currentIndex;
+        if (plus) targetindex++;
+        else targetindex--;
+        ShowPanel(targetindex);
     }
-
-    //public void OnLeftButton()
-    //{
-    //    currentIndex--; //왼쪽으로 이동
-
-    //    // 인덱스가 0보다 작아지면 맨 끝으로
-    //    if (currentIndex < 0)
-    //    {
-    //        currentIndex = sprites.Length - 1;
-    //    }
-
-    //    image.sprite = sprites[currentIndex];
-    //}
-    //public void OnRightButton()
-    //{
-    //    currentIndex++; //오른쪽으로 이동
-
-    //    // 마지막 이미지 이후면 맨 앞으로
-    //    if (currentIndex >= sprites.Length)
-    //    {
-    //        currentIndex = 0;
-    //    }
-
-    //    image.sprite = sprites[currentIndex];
-    //}
-
-
-    public void ShowPanel(int index)
+    private void ShowPanel(int index)
     {
         // 슬라이딩 중이거나 같은 패널이면 무시
         if (index == currentIndex || isSliding) return;
+
+        // 이동 방향 계산 (왼쪽/오른쪽)
+        Vector2 panelPosition = currentIndex > index ? new Vector2(slideDistance, 0) : new Vector2(-slideDistance, 0);
 
         // 인덱스 범위 벗어나면 순환 처리
         if (index >= panels.Length) index = 0;
@@ -68,8 +49,7 @@ public class Gunimage : MonoBehaviour
         fromPanel.transform.SetAsLastSibling();
         toPanel.transform.SetAsFirstSibling();
 
-        // 이동 방향 계산 (왼쪽/오른쪽)
-        Vector2 panelPosition = currentIndex > index ? new Vector2(Screen.width, 0) : new Vector2(-Screen.width, 0);
+       
 
         // 새 패널 위치 지정 및 활성화
         toPanel.gameObject.SetActive(true);
@@ -77,12 +57,13 @@ public class Gunimage : MonoBehaviour
 
         // DOTween 애니메이션
         fromPanel.DOAnchorPos(-panelPosition, slideDuration).SetEase(Ease.OutCubic);
+
         toPanel.DOAnchorPos(Vector2.zero, slideDuration).SetEase(Ease.OutCubic)
             .OnComplete(() =>
             {
                 // 이전 패널 비활성화 및 리셋
                 fromPanel.gameObject.SetActive(false);
-                fromPanel.anchoredPosition = Vector2.zero;
+                //fromPanel.anchoredPosition = Vector2.zero;
                 currentIndex = index;
                 isSliding = false;
             });
