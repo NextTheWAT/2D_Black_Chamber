0d0ccfa36b48a4be5c6c98853586e053ae3f45e8
Lee JaeEun <62467424+NextTheWAT@users.noreply.github.com>
2025-10-29 16:34:56 +0900

    [FIx] Wall,Door,SteelDoor Attack 사운드

--- Assets/02. Scripts/Weapon/Bullet/Bullet.Sound.cs
+++ Assets/02. Scripts/Weapon/Bullet/Bullet.Sound.cs
@@ -3,10 +3,24 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
 public partial class Bullet : MonoBehaviour
 {
+    [SerializeField] private LayerMask wallSoundLayer;
+    [SerializeField] private LayerMask doorSoundLayer;
+    [SerializeField] private LayerMask steelDoorSoundLayer;
+
+
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Wall")) StructSoundManager.Instance.PlayWallAttackSound(transform.position);
-        else if(other.CompareTag("Door")) StructSoundManager.Instance.PlayDoorAttackSound(transform.position);
-        else if(other.CompareTag("SteelDoor")) StructSoundManager.Instance.PlaySteelDoorAttackSound(transform.position);
+        if (other.gameObject.layer == wallSoundLayer) {
+            StructSoundManager.Instance.PlayWallAttackSound(transform.position);
+        }
+        else if (other.gameObject.layer == doorSoundLayer) 
+        {
+            StructSoundManager.Instance.PlayDoorAttackSound(transform.position);
+        }
+        else if (other.gameObject.layer == steelDoorSoundLayer) 
+        {
+            StructSoundManager.Instance.PlaySteelDoorAttackSound(transform.position);
+        }
     }
 }
